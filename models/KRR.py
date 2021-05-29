5b64c892b7b15c3290453d2e5ac442321d6e7559
SahasG <sahasgelli@Sahass-MBP.attlocal.net>
2021-05-29 15:11:41 -0500

    KRR with correct scores and cross validation is being worked on

--- models/KRR.py
+++ models/KRR.py
@@ -1,15 +1,20 @@
+
 from sklearn.kernel_ridge import KernelRidge
+from matplotlib import pyplot
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 import sqlite3
 import numpy as np
+import csv
+import math
 from rdkit import Chem
 from rdkit.Chem import MACCSkeys
+from sklearn.model_selection import cross_validate
 
 def Transform_Data():
     
-    SMILESdata, Atomizationdata = readSqliteTable()
+    SMILESdata, GapData = readCSV('data/qm9.csv')
     SMILESstrings = list(SMILESdata.keys())
     FeatureVector = []
     BadParticles = []
@@ -26,71 +31,55 @@ def Transform_Data():
     print(X.shape)
     print(Z.shape)
     for keys in BadParticles:
-        del Atomizationdata[keys]
-    AtomizationE = list(Atomizationdata.values())
-    y = np.array(AtomizationE[:25000])
-    w = np.array(AtomizationE[25000:50000])
-    print(y.shape)
-    print(w.shape)
-    return X, y, Z, w
-
-
+        del GapData[keys]
+    GapData = list(GapData.values())
+    y3 = np.array(GapData[:25000])
+    w3 = np.array(GapData[25000:50000])
+    return X, Z, w3, y3
 
 def split(bitString):
     
     return [int(char) for char in bitString] 
+
+def readCSV(filepath):
+
+    Smile_Strings = {}
+    Gap_Id = {}
+    with open(filepath, newline = '') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Smile_Strings[row['mol_id']] = row['smiles']
+            Gap_Id[row['mol_id']] = float(row['gap'])
+    
+    return Smile_Strings, Gap_Id
+    
+
     
 def Create_Model():
     
-    model = KernelRidge(kernel='laplacian')
-    X, y, Z, w = Transform_Data()
-    model.fit(X, y)
-    predict = model.predict(Z)
-    print(predict[1])
-    R_squared = r2_score(predict, w)
-    RMSE = mean_squared_error(predict, w)
-    MAE = mean_absolute_error(predict, w)
-    print("The R^2 score is: ",R_squared)
-    print("The RMSE score is: ", RMSE)
-    print("The MAE score is: ", MAE)
+    model3 = KernelRidge(kernel='laplacian')
+    X, Z, w3, y3= Transform_Data()
+    model3.fit(X, y3)
+    predict3 = model3.predict(Z)
+
+    c = np.random.rand(25000)
+    pyplot.scatter(w3, predict3, c=c)
+    pyplot.ylabel('Test Data')
+    pyplot.xlabel('Predict Data')
+    pyplot.title('Gap')
+    pyplot.show()
     
+    cv_results = cross_validate(model3, X, y3, cv=3)
+    sorted(cv_results.keys())
+    print(cv_results['test_score'])
     
-
-def readSqliteTable():
-    try:
-        sqliteConnection = sqlite3.connect('./data/g4mp2-gdb9.db')
-        cursor = sqliteConnection.cursor()
-        print("Connected to SQLite")
-
-        sqlite_select_query = """SELECT * from text_key_values"""
-        cursor.execute(sqlite_select_query)
-        records = cursor.fetchall()
-        SMILESstrings = {}
-        SMILESkeys = {}
-        for row in records:
-            if(row[0] == 'Smiles'):
-                SMILESstrings[row[2]] = row[1]
-                
-        sqlite_select_query = """SELECT * from number_key_values"""
-        cursor.execute(sqlite_select_query)
-        records = cursor.fetchall()
-        AtomizationE = {}
-        AtomizationE2 = {}
-        for row in records:
-            if(row[0] == 'g4mp2_AtomizationE'):
-                AtomizationE[row[2]] = row[1]
-        
-        
-        cursor.close()
-        
-        
-        return SMILESstrings, AtomizationE
-
-    except sqlite3.Error as error:
-        print("Failed to read data from sqlite table", error)
-    finally:
-        if (sqliteConnection):
-            sqliteConnection.close()
-            print("The SQLite connection is closed")
+    R_squared3 = r2_score(w3, predict3)
+    RMSE3 = math.sqrt(mean_squared_error(w3, predict3))
+    MAE3 = mean_absolute_error(w3, predict3)
+    print("The R^2 score of GAP model is: ",R_squared3)
+    print("The RMSE score of GAP model is: ", RMSE3)
+    print("The MAE score of GAP model is: ", MAE3)
             
-Create_Model()
+if __name__ == "__main__":
+    Create_Model()
+
